7561459431c0875ba967ea71759a64fb7ec2dcbe
eskat0n <muyou.prj@gmail.com>
2012-09-14 14:26:09 +0600

    added task to build script for solution building

--- build/default.ps1
+++ build/default.ps1
@@ -1,11 +1,26 @@
 Framework '4.0'
 
 properties {	
-	
+	$buildNumber = $null
+    $configuration = $null
+
+    $solutionPath = '../'
+	$solutionFilename = $solutionPath + 'MonoTorrent.sln'
 }
 
 task default -depends DoNotRunItDirectly
 
 task DoNotRunItDirectly {
 	'Do not run it directly!'
+}
+
+task CommonBuild -depends EnsureParams, Build
+
+task EnsureParams {
+	Assert ($buildNumber -ne $null) 'Property `$buildNumber should be specified'`
+	Assert ($configuration -ne $null) 'Property `$configuration should be specified'`
+}
+
+task Build {
+    MSBuild -t:Build -p:Configuration=$configuration $solutionFilename  
 }
