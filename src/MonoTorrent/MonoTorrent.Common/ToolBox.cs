15de1334040890bfca6835479b32171c7f56bf0b
eskat0n <muyou.prj@gmail.com>
2012-09-20 11:23:39 +0600

    refactoring for ToolBox utility class

--- src/MonoTorrent/MonoTorrent.Common/ToolBox.cs
+++ src/MonoTorrent/MonoTorrent.Common/ToolBox.cs
@@ -26,42 +26,29 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-
-
-using System;
-using System.Collections;
-using System.Text;
-using System.Collections.Generic;
-using System.Threading;
-using MonoTorrent.Client.Encryption;
-
 namespace MonoTorrent.Common
 {
-	public delegate long Operation<T>(T target);
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using MonoTorrent.Client.Encryption;
+
+    public delegate long Operation<in T>(T target);
 
     public static class Toolbox
     {
-        private static Random r = new Random();
-		public static int Count<T>(IEnumerable<T> enumerable, Predicate<T> predicate)
-		{
-			int count = 0;
+        private static readonly Random Random = new Random();
 
-			foreach (T t in enumerable)
-				if (predicate(t))
-					count++;
+        public static int Count<T>(IEnumerable<T> enumerable, Predicate<T> predicate)
+        {
+            return enumerable.Count(t => predicate(t));
+        }
 
-			return count;
-		}
-
-		public static long Accumulate<T>(IEnumerable<T> enumerable, Operation<T> action)
-		{
-            long count = 0;
-
-			foreach (T t in enumerable)
-				count += action(t);
-		
-			return count;
-		}
+        public static long Accumulate<T>(IEnumerable<T> enumerable, Operation<T> action)
+        {
+            return enumerable.Sum(t => action(t));
+        }
 
         public static void RaiseAsyncEvent<T>(EventHandler<T> e, object o, T args)
             where T : EventArgs
@@ -69,49 +56,47 @@ namespace MonoTorrent.Common
             if (e == null)
                 return;
 
-            ThreadPool.QueueUserWorkItem(delegate {
-                if (e != null)
-                    e(o, args);
-            });
+            ThreadPool.QueueUserWorkItem(delegate { e(o, args); });
         }
+
         /// <summary>
-        /// Randomizes the contents of the array
+        ///   Randomizes the contents of the array
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="array"></param>
+        /// <typeparam name="T"> </typeparam>
+        /// <param name="array"> </param>
         public static void Randomize<T>(List<T> array)
         {
-            List<T> clone = new List<T>(array);
+            var clone = new List<T>(array);
             array.Clear();
 
             while (clone.Count > 0)
             {
-                int index = r.Next(0, clone.Count);
+                var index = Random.Next(0, clone.Count);
                 array.Add(clone[index]);
                 clone.RemoveAt(index);
             }
         }
 
         /// <summary>
-        /// Switches the positions of two elements in an array
+        ///   Switches the positions of two elements in an array
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="array"></param>
-        /// <param name="first"></param>
-        /// <param name="second"></param>
+        /// <typeparam name="T"> </typeparam>
+        /// <param name="array"> </param>
+        /// <param name="first"> </param>
+        /// <param name="second"> </param>
         public static void Switch<T>(IList<T> array, int first, int second)
         {
-            T obj = array[first];
+            var obj = array[first];
             array[first] = array[second];
             array[second] = obj;
         }
 
         /// <summary>
-        /// Checks to see if the contents of two byte arrays are equal
+        ///   Checks to see if the contents of two byte arrays are equal
         /// </summary>
-        /// <param name="array1">The first array</param>
-        /// <param name="array2">The second array</param>
-        /// <returns>True if the arrays are equal, false if they aren't</returns>
+        /// <param name="array1"> The first array </param>
+        /// <param name="array2"> The second array </param>
+        /// <returns> True if the arrays are equal, false if they aren't </returns>
         public static bool ByteMatch(byte[] array1, byte[] array2)
         {
             if (array1 == null)
@@ -126,14 +111,14 @@ namespace MonoTorrent.Common
         }
 
         /// <summary>
-        /// Checks to see if the contents of two byte arrays are equal
+        ///   Checks to see if the contents of two byte arrays are equal
         /// </summary>
-        /// <param name="array1">The first array</param>
-        /// <param name="array2">The second array</param>
-        /// <param name="offset1">The starting index for the first array</param>
-        /// <param name="offset2">The starting index for the second array</param>
-        /// <param name="count">The number of bytes to check</param>
-        /// <returns></returns>
+        /// <param name="array1"> The first array </param>
+        /// <param name="array2"> The second array </param>
+        /// <param name="offset1"> The starting index for the first array </param>
+        /// <param name="offset2"> The starting index for the second array </param>
+        /// <param name="count"> The number of bytes to check </param>
+        /// <returns> </returns>
         public static bool ByteMatch(byte[] array1, int offset1, byte[] array2, int offset2, int count)
         {
             if (array1 == null)
@@ -146,7 +131,7 @@ namespace MonoTorrent.Common
                 return false;
 
             // Check if any elements are unequal
-            for (int i = 0; i < count; i++)
+            for (var i = 0; i < count; i++)
                 if (array1[offset1 + i] != array2[offset2 + i])
                     return false;
 
