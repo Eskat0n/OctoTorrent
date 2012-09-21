bb495af34227e04a5ffab44ca405b3ebad05b318
eskat0n <muyou.prj@gmail.com>
2012-09-21 11:01:32 +0600

    implemented AzureusStyleIdentifierProvider

--- src/MonoTorrent/MonoTorrent.Common/Identification/MonoTorrentIdentifierProvider.cs
+++ src/MonoTorrent/MonoTorrent.Common/Identification/MonoTorrentIdentifierProvider.cs
@@ -4,38 +4,26 @@ namespace MonoTorrent.Common
     using System.Linq;
     using System.Reflection;
 
-    public sealed class MonoTorrentIdentifierProvider : IIdentifierProvider
+    public sealed class MonoTorrentIdentifierProvider : AzureusStyleIdentifierProvider
     {
-        public string CreatePeerId()
+        public MonoTorrentIdentifierProvider()
+            : base("MO")
         {
-            var version = GetAssemblyVersion();
-
-            // 'MO' for MonoTorrent then four digit version number
-            var versionString = string.Format("{0}{1}{2}{3}",
-                                              Math.Max(version.Major, 0),
-                                              Math.Max(version.Minor, 0),
-                                              Math.Max(version.Build, 0),
-                                              Math.Max(version.Revision, 0));
-            versionString = versionString.Length > 4
-                                ? versionString.Substring(0, 4)
-                                : versionString.PadRight(4, '0');
-
-            return string.Format("-MO{0}-", versionString);
         }
 
-        public string CreateHumanReadableId()
+        public override string CreateHumanReadableId()
         {
-            return string.Format("MonoTorrent {0}", GetAssemblyVersion());
+            return string.Format("MonoTorrent {0}", GetVersion());
         }
 
-        public string CreateDhtClientVersion()
+        public override string CreateDhtClientVersion()
         {
             return "MO06";
         }
 
-        private static Version GetAssemblyVersion(Assembly assembly = null)
+        protected override Version GetVersion()
         {
-            var monotorrentAssembly = assembly ?? Assembly.GetExecutingAssembly();
+            var monotorrentAssembly = Assembly.GetExecutingAssembly();
             var versionAttribute = GetAssemblyAttribute<AssemblyInformationalVersionAttribute>(monotorrentAssembly);
 
             return new Version(versionAttribute.InformationalVersion);
