95fbd92fc77da96f651e08c64f3acada00bb5861
eskat0n <muyou.prj@gmail.com>
2012-10-16 16:47:28 +0600

    AssemblyInfos updated

--- src/MonoTorrent.Dht/AssemblyInfo.cs
+++ src/MonoTorrent.Dht/AssemblyInfo.cs
@@ -2,10 +2,10 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("OctoTorrent.Tests")]
-[assembly: AssemblyCopyright("Alan McGovern, Olivier Dufour 2009")]
-[assembly: AssemblyDescription("MonoTorrent DHT")]
-[assembly: AssemblyFileVersion("0.70")]
-[assembly: AssemblyInformationalVersion("0.70")]
-[assembly: AssemblyProduct("MonoTorrent DHT")]
-[assembly: AssemblyTitle("MonoTorrent DHT")]
-[assembly: AssemblyVersion("0.70")]
+[assembly: AssemblyCopyright("Timur Rakhmatillaev, Alan McGovern, Olivier Dufour 2009; MIT X11 Licensed")]
+[assembly: AssemblyDescription("OctoTorrent DHT (based on MonoTorrent)")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: AssemblyProduct("OctoTorrent DHT")]
+[assembly: AssemblyTitle("OctoTorrent DHT")]
+[assembly: AssemblyVersion("0.1.0")]
