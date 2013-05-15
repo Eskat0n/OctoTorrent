65617c542a9c225a0421040cc835b2eeb0a62b7f
eskat0n <muyou.prj@gmail.com>
2013-05-15 15:28:03 +0600

    cleanup

--- sources/OctoTorrent.Tests/Dht/RoutingTableTests.cs
+++ sources/OctoTorrent.Tests/Dht/RoutingTableTests.cs
@@ -9,44 +9,36 @@ namespace OctoTorrent.Tests.Dht
     [TestFixture]
     public class RoutingTableTests
     {
-        //static void Main(string[] args)
-        //{
-        //    RoutingTableTests t = new RoutingTableTests();
-        //    t.Setup();
-        //    t.AddSame();
-        //    t.Setup();
-        //    t.AddSimilar();
-        //}
-        byte[] id;
-        RoutingTable table;
-        Node n;
-        int addedCount;
+        private byte[] _id;
+        private RoutingTable _table;
+        private Node _node;
+        private int _addedCount;
         
         [SetUp]
         public void Setup()
         {
-            id = new byte[20];
-            id[1] = 128;
-            n = new Node(new NodeId(id), new System.Net.IPEndPoint(IPAddress.Any, 0));
-            table = new RoutingTable(n);
-            table.NodeAdded += delegate { addedCount++; };
-            table.Add(n);//the local node is no more in routing table so add it to show test is still ok
-            addedCount = 0;
+            _id = new byte[20];
+            _id[1] = 128;
+            _node = new Node(new NodeId(_id), new IPEndPoint(IPAddress.Any, 0));
+            _table = new RoutingTable(_node);
+            _table.NodeAdded += delegate { _addedCount++; };
+            _table.Add(_node); //the local node is no more in routing table so add it to show test is still ok
+            _addedCount = 0;
         }
 
         [Test]
         public void AddSame()
         {
-            table.Clear();
-            for (int i = 0; i < Bucket.MaxCapacity; i++)
+            _table.Clear();
+            for (var i = 0; i < Bucket.MaxCapacity; i++)
             {
-                byte[] id = (byte[])this.id.Clone();
-                table.Add(new Node(new NodeId(id), new IPEndPoint(IPAddress.Any, 0)));
+                var id = (byte[])_id.Clone();
+                _table.Add(new Node(new NodeId(id), new IPEndPoint(IPAddress.Any, 0)));
             }
 
-            Assert.AreEqual(1, addedCount, "#a");
-            Assert.AreEqual(1, table.Buckets.Count, "#1");
-            Assert.AreEqual(1, table.Buckets[0].Nodes.Count, "#2");
+            Assert.AreEqual(1, _addedCount, "#a");
+            Assert.AreEqual(1, _table.Buckets.Count, "#1");
+            Assert.AreEqual(1, _table.Buckets[0].Nodes.Count, "#2");
 
             CheckBuckets();
         }
@@ -54,21 +46,21 @@ namespace OctoTorrent.Tests.Dht
         [Test]
         public void AddSimilar()
         {
-            for (int i = 0; i < Bucket.MaxCapacity * 3; i++)
+            for (var i = 0; i < Bucket.MaxCapacity * 3; i++)
             {
-                byte[] id = (byte[])this.id.Clone();
+                var id = (byte[])_id.Clone();
                 id[0] += (byte)i;
-                table.Add(new Node(new NodeId(id), new IPEndPoint(IPAddress.Any, 0)));
+                _table.Add(new Node(new NodeId(id), new IPEndPoint(IPAddress.Any, 0)));
             }
 
-            Assert.AreEqual(Bucket.MaxCapacity * 3 - 1, addedCount, "#1");
-            Assert.AreEqual(6, table.Buckets.Count, "#2");
-            Assert.AreEqual(8, table.Buckets[0].Nodes.Count, "#3");
-            Assert.AreEqual(8, table.Buckets[1].Nodes.Count, "#4");
-            Assert.AreEqual(8, table.Buckets[2].Nodes.Count, "#5");
-            Assert.AreEqual(0, table.Buckets[3].Nodes.Count, "#6");
-            Assert.AreEqual(0, table.Buckets[4].Nodes.Count, "#7");
-            Assert.AreEqual(0, table.Buckets[5].Nodes.Count, "#8");
+            Assert.AreEqual(Bucket.MaxCapacity * 3 - 1, _addedCount, "#1");
+            Assert.AreEqual(6, _table.Buckets.Count, "#2");
+            Assert.AreEqual(8, _table.Buckets[0].Nodes.Count, "#3");
+            Assert.AreEqual(8, _table.Buckets[1].Nodes.Count, "#4");
+            Assert.AreEqual(8, _table.Buckets[2].Nodes.Count, "#5");
+            Assert.AreEqual(0, _table.Buckets[3].Nodes.Count, "#6");
+            Assert.AreEqual(0, _table.Buckets[4].Nodes.Count, "#7");
+            Assert.AreEqual(0, _table.Buckets[5].Nodes.Count, "#8");
             CheckBuckets();
         }
 
@@ -76,20 +68,21 @@ namespace OctoTorrent.Tests.Dht
         public void GetClosestTest()
         {
             List<NodeId> nodes;
-            TestHelper.ManyNodes(out table, out nodes);
+            TestHelper.ManyNodes(out _table, out nodes);
             
+            var closest = _table.GetClosest(_table.LocalNode.Id);
 
-            List<Node> closest = table.GetClosest(table.LocalNode.Id);
             Assert.AreEqual(8, closest.Count, "#1");
-            for (int i = 0; i < 8; i++)
-                Assert.IsTrue(closest.Exists(delegate(Node node) { return nodes[i].Equals(closest[i].Id); }));
+
+            for (var i = 0; i < 8; i++)
+                Assert.IsTrue(closest.Exists(node => nodes[i].Equals(closest[i].Id)));
         }
         
         private void CheckBuckets()
         {
-            foreach (Bucket b in table.Buckets)
-                foreach (Node n in b.Nodes)
-                    Assert.IsTrue(n.Id >= b.Min && n.Id < b.Max);
+            foreach (var bucket in _table.Buckets)
+                foreach (var node in bucket.Nodes)
+                    Assert.IsTrue(node.Id >= bucket.Min && node.Id < bucket.Max);
         }
     }
 }
